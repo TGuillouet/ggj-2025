17a3a702daee178fd8a5df2ced3e1e0e71652ecb
Thomas Guillouët <thomas.guillouet@outlook.com>
2025-01-23 23:20:47 +0100

    Add some dependencies

---
+++ Justfile
@@ -0,0 +1,6 @@
+start-dev:
+  cargo run --features bevy/dynamic_linking
+
+start-rel:
+  cargo run --features bevy/dynamic_linking --release
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use bevy::{prelude::*, window::WindowResolution};
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn(Sprite::from_image(asset_server.load("bevy_logo_dark.png")));
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Game - Floating".to_string(),
+                        resolution: WindowResolution::new(1600.0, 900.0),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, setup)
+        .run();
 }
