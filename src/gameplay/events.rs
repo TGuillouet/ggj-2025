9662c3fe3d25b6402f7e26e78ed79d1956ac559b
Thomas Guillouët <thomas.guillouet@outlook.com>
2025-01-26 00:05:53 +0100

    Fix warnings

--- src/gameplay/events.rs
+++ src/gameplay/events.rs
@@ -2,7 +2,3 @@ use bevy::prelude::*;
 
 #[derive(Event, Default)]
 pub struct WinEvent;
-
-// This event is for the ui to update the progression of the player
-#[derive(Event)]
-pub struct TimePassedEvent;
