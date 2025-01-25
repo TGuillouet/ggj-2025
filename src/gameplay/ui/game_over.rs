5eda8e3aec6cb5984b912d165f38d06a589348c3
Thomas Guillouët <thomas.guillouet@outlook.com>
2025-01-25 22:43:27 +0100

    Add the continue text

--- src/gameplay/ui/game_over.rs
+++ src/gameplay/ui/game_over.rs
@@ -10,6 +10,8 @@ pub fn spawn_menu(mut commands: Commands, assets_server: Res<AssetServer>) {
                 justify_content: JustifyContent::Center,
                 align_items: AlignItems::Center,
                 overflow: Overflow::visible(),
+                flex_direction: FlexDirection::Column,
+                row_gap: Val::Percent(15.0),
                 ..default()
             },
             BackgroundColor(Color::BLACK),
@@ -21,6 +23,22 @@ pub fn spawn_menu(mut commands: Commands, assets_server: Res<AssetServer>) {
                     // This font is loaded and will be used instead of the default font.
                     font: assets_server.load("VT323-Regular.ttf"),
                     font_size: 67.0,
+                    ..default()
+                },
+                // Set the justification of the Text
+                TextLayout::new_with_justify(JustifyText::Center),
+                Node {
+                    width: Val::Percent(100.0),
+                    ..default()
+                },
+            ));
+
+            children.spawn((
+                Text::new("Appuyez sur une touche pour continuer !"),
+                TextFont {
+                    // This font is loaded and will be used instead of the default font.
+                    font: assets_server.load("VT323-Regular.ttf"),
+                    font_size: 30.0,
                     ..default()
                 },
                 // Set the justification of the Text
